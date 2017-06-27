f7aeac57a8d180947c61e984c49d01e24926b909
duck84 <michael.mcgrath23@yahoo.com>
2017-06-26 23:11:12 -0700

    updates

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Mike and Lidija's Guide to Portland - Map Project
+# Udacity's Neighborhood Map Project
+## Mike and Lidija's Guide to Portland
+
+This is a project for the Udacity Front-End Web Developer Nanodegree. The goal is to create a singe page app using the Google API and another third party API (I have chosen FourSquare). 
+
+The app allows you to search the listings and display the locations of each place. When clicked, more information will be displayed.
+
+On the backend, the programming also uses Knockout.js to manage the data structure.
+
+Here is the link to the site https://duck84.github.io/map/.
+
+### Sources
+
+* http://knockoutjs.com/documentation/introduction.html
+* https://developers.google.com/maps/documentation/javascript/tutorial
+* https://developer.foursquare.com/overview/
+
+Special thanks to:
+* The Udacity Forums and espically Karol
+* Stack Overflow
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 var map;
+function initMap(){
+}
 
 //create a variable for markers
 var markers = [];
@@ -19,8 +21,6 @@ var viewModel = {
 
 };
 
-callFoursquare()
-
 //code that runs the search filter
 viewModel.Query = ko.observable('');
 viewModel.searchResults = ko.computed(function() {
@@ -32,7 +32,7 @@ viewModel.searchResults = ko.computed(function() {
 
 //code that links the filter to the markers
 viewModel.test = ko.computed(function() {
-  if (viewModel.Query() === "") {
+  if (viewModel.Query() == false) {
     showListings();
   } else {
   hideMarkers(markers);
@@ -51,7 +51,6 @@ var results = viewModel.searchResults();
 
 ko.applyBindings(viewModel);
 
-
 //creates map with markers
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -59,30 +58,29 @@ function initMap() {
     zoom: 12
   });
 
-
-
 var locations = viewModel.locations;
 
 var infowindow = new google.maps.InfoWindow();
+  
+for (var i = 0; i < locations.length; i++) {
 
-locations.forEach(function(element, index, array) {
+  (function(j) {
 
-
-    var fsdata = viewModel.locations[index].data; //foursquare data that contains user and vist count for each location
-    var position = locations[index].location;
-    var title = locations[index].title;
+    var fsdata = viewModel.locations[j].data; //foursquare data that contains user and vist count for each location
+    var position = locations[j].location;
+    var title = locations[j].title;
     var marker = new google.maps.Marker({
       position: position,
       title: title,
       animation: google.maps.Animation.DROP,
-      id: index,
+      id: j,
       visible: true
     });
     markers.push(marker);
 
-    markers[index].setMap(map);
-    viewModel.locations[index].marker = marker;
-    viewModel.locations[index].infowindow = infowindow;
+    markers[i].setMap(map);
+    viewModel.locations[i].marker = marker;
+    viewModel.locations[i].infowindow = infowindow;
 
     marker.addListener('click', function() {
       infowindow.setContent(title + '<br><br>' +
@@ -98,7 +96,8 @@ locations.forEach(function(element, index, array) {
 
     });
 
-  });
+  })(i);
+}
 }
 
 // This function will loop through the markers array and display them all.
@@ -128,18 +127,19 @@ function callFoursquare() {
   var client_secret = "BBCJDCJTIKPJZURDRBPBVWR1ESGEBSU4O3OJEA5UK0LUF4LA";
   var client_id = "HSFBG34JW1X1KJEF4DIBLJDWSXRNWJ1I5VCFR4RKOALCJ1AS";
 
-  viewModel.locations.forEach(function(element, index, array) {
-    var lat = viewModel.locations[index].location.lat;
-    var lng = viewModel.locations[index].location.lng;
+  for (var i = 0; i < viewModel.locations.length; i++) {
+    var lat = viewModel.locations[i].location.lat;
+    var lng = viewModel.locations[i].location.lng;
     var limit = 1;
     var fsURL = "https://api.foursquare.com/v2/venues/search?v=" + version + "&ll=" + lat + "," + lng + "&client_id=" + client_id + "&client_secret=" + client_secret + "&limit=" + limit;
 
-    $.getJSON(fsURL, function (data) {
-      var jsonObject = data
-      viewModel.locations[index].data = jsonObject.response.venues[0].stats;
-    });
-
-})
-};
-
-//TODO get the callFoursquare function to work. Alternative is to hard code foursquare IDs
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", fsURL, false);
+    xhttp.send(null);
+    var jsonObject = JSON.parse(xhttp.responseText);
+    
+   
+    viewModel.locations[i].data = jsonObject.response.venues[0].stats;
+}
+}
+callFoursquare()
