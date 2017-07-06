49d991694acf0029104c46a6ad3cdd209f57cd90
duck84 <michael.mcgrath23@yahoo.com>
2017-07-05 22:48:55 -0700

    fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 var map;
 //create a variable for markers
 var markers = [];
-var viewModel = {
+var ViewModel = {
 	show: function(self) {
 		google.maps.event.trigger(self.marker, 'click');
 	},
@@ -50,29 +50,29 @@ var viewModel = {
 	}, ],
 };
 //code that runs the search filter
-viewModel.Query = ko.observable('');
-viewModel.searchResults = ko.computed(function() {
-	var query = viewModel.Query();
-	return viewModel.locations.filter(function(item) {
+ViewModel.Query = ko.observable('');
+ViewModel.searchResults = ko.computed(function() {
+	var query = ViewModel.Query();
+	return ViewModel.locations.filter(function(item) {
 		return item.title.toLowerCase().indexOf(query) >= 0;
 	});
 });
 //code that links the filter to the markers
-viewModel.test = ko.computed(function() {
-	if (viewModel.Query() === "") {
-		viewModel.showListings();
+ViewModel.filtered = ko.computed(function() {
+	if (ViewModel.Query() === "") {
+		ViewModel.showListings();
 	} else {
-		hideMarkers(markers);
+		ViewModel.hideMarkers(markers);
 		//    var bounds = new google.maps.LatLngBounds();
-		for (var i = 0; i < viewModel.searchResults().length; i++) {
-			viewModel.searchResults()[i].marker.setVisible(true);
-			//  bounds.extend(viewModel.searchResults()[i].marker.position);
+		for (var i = 0; i < ViewModel.searchResults().length; i++) {
+			ViewModel.searchResults()[i].marker.setVisible(true);
+			//  bounds.extend(ViewModel.searchResults()[i].marker.position);
 		}
 		//    map.fitBounds(bounds);
 	}
 });
-var results = viewModel.searchResults();
-ko.applyBindings(viewModel);
+var results = ViewModel.searchResults();
+ko.applyBindings(ViewModel);
 //creates map with markers
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -82,65 +82,63 @@ function initMap() {
 		},
 		zoom: 12
 	});
+  infowindow = new google.maps.InfoWindow();
 }
 
-function initMarkers() {
-	var locations = viewModel.locations;
-	var infowindow = new google.maps.InfoWindow();
-	locations.forEach(function(element, index, array) {
-		var fsdata = viewModel.locations[index].data; //foursquare data that contains user and vist count for each location
-		var position = locations[index].location;
-		var title = locations[index].title;
-		var marker = new google.maps.Marker({
-			position: position,
-			title: title,
-			animation: google.maps.Animation.DROP,
-			id: index,
-			visible: true
-		});
-		markers.push(marker);
-		markers[index].setMap(map);
-		viewModel.locations[index].marker = marker;
-		viewModel.locations[index].infowindow = infowindow;
-		marker.addListener('click', function() {
-			infowindow.setContent(title + '<br><br>' + 'FourSquare users: ' + fsdata.usersCount + '<br> ' + 'FourSquare visits: ' + fsdata.checkinsCount);
-			infowindow.open(map, this);
-			map.setZoom(12);
-			map.setCenter(marker.getPosition());
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-			setTimeout(function() {
-				marker.setAnimation(null);
-			}, 2100);
-		});
+var markerCount = 0;
+
+function createMarkers(data) {
+	var locations = ViewModel.locations;
+	var fsdata = data; //foursquare data that contains user and vist count for each location
+	var position = locations[markerCount].location;
+	var title = locations[markerCount].title;
+	var marker = new google.maps.Marker({
+		position: position,
+		title: title,
+		animation: google.maps.Animation.DROP,
+		id: markerCount,
+		visible: true
 	});
+	markers.push(marker);
+	markers[markerCount].setMap(map);
+	ViewModel.locations[markerCount].marker = marker;
+	marker.addListener('click', function() {
+		infowindow.setContent(title + '<br><br>' + 'FourSquare users: ' + fsdata.usersCount + '<br> ' + 'FourSquare visits: ' + fsdata.checkinsCount);
+		infowindow.open(map, this);
+		map.setZoom(12);
+		map.setCenter(marker.getPosition());
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+		setTimeout(function() {
+			marker.setAnimation(null);
+		}, 2100);
+	});
+	markerCount ++;
+	return markerCount
 }
+
 //funcation that requests data from FourSquare's API and saves it for later use.
 function callFoursquare() {
 	var version = "20170601";
 	var client_secret = "BBCJDCJTIKPJZURDRBPBVWR1ESGEBSU4O3OJEA5UK0LUF4LA";
 	var client_id = "HSFBG34JW1X1KJEF4DIBLJDWSXRNWJ1I5VCFR4RKOALCJ1AS";
-	viewModel.locations.forEach(function(element, index, array) {
-		var lat = viewModel.locations[index].location.lat;
-		var lng = viewModel.locations[index].location.lng;
+	ViewModel.locations.forEach(function(element, index, array) {
+		var lat = ViewModel.locations[index].location.lat;
+		var lng = ViewModel.locations[index].location.lng;
 		var limit = 1;
 		var fsURL = "https://api.foursquare.com/v2/venues/search?v=" + version + "&ll=" + lat + "," + lng + "&client_id=" + client_id + "&client_secret=" + client_secret + "&limit=" + limit;
 		fetch(fsURL).then(function(response) {
 			if (response.status !== 200) {
-				console.log('Looks like there was a problem. Status Code: ' + response.status);
+				alert('Looks like there was a problem. Status Code: ' + response.status);
 				return;
 			}
-			// Examine the text in the response
+			// Adds the FourSquare data to the createMarker function and creates makers
 			response.json().then(function(data) {
-				viewModel.locations[index].data = data.response.venues[0].stats;
-				console.log(data);
+        createMarkers(data.response.venues[0].stats)
 			});
 		}).catch(function(err) {
-			console.log('Fetch Error :-S', err);
+      alert('Fetch Error :-S', err);
 		});
 	});
-	setTimeout(function() {
-		initMarkers();
-	}, 1000);
 }
 callFoursquare();
 	//TODO get the callFoursquare function to work. Alternative is to hard code foursquare IDs
